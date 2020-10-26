eb298e38b1cf14c75ca77b18efc769edf5cb79b0
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-10-26 14:52:19 -0700

    Initial commit of keybinding to create heap shapshots

--- packages/core/parcel/src/cli.js
+++ packages/core/parcel/src/cli.js
@@ -253,6 +253,9 @@ async function run(entries: Array<string>, command: any) {
             ? parcel.stopProfiling()
             : parcel.startProfiling());
           break;
+        case 'y':
+          await parcel.takeHeapSnapshot();
+          break;
       }
     });
 
